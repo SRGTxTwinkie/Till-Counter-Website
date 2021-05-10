da5be515b12cb65eb812f9e44e9faaf75b9baf7e
SRGTxTwinkie <54576833+SRGTxTwinkie@users.noreply.github.com>
2021-05-10 02:53:58 -0400

    comments on JS file

--- scripts.js
+++ scripts.js
@@ -1,11 +1,21 @@
+// The id_names holds all the class names of the input boxes for looping in the future
+// I can think of 100 different ways to do this now that would work better
 var id_names = ["20_amount", "10_amount", "5_amount", "1_amount", "quarter_amount", "dime_amount", "nickel_amount", "penny_amount", "pennyp_amount", "nicklep_amount", "dimep_amount", "quarterp_amount"];
+
+// The dollar amount of the bills and coins in order based on the id_names array above
 var amounts = [20.0, 10.0, 5.0, 1.0, 0.25, 0.10, 0.05, 0.01, 0.50, 2.00, 5.00, 10.00];
+
+// This hold all the totals in order based on the id_names array
+// 100 ways on this too
 var totals = [];
 
+// This function is a boiler plate to avoid typing document.getElementById("element")
+// You just call this function and do what you want with the element
 function grabElement(element_name){    
     return document.getElementById(element_name);
 }
 
+// This function "Zeros" out all the input fields on the form by changing the values to "", a blank string
 function clearAll(){
     for(let i = 0; i < id_names.length; i++){
         grabElement(id_names[i]).value = '';
@@ -13,6 +23,7 @@ function clearAll(){
     }
 }
 
+// This function parses numbers from strings, or in JavaScript's case also from NaN - not a number
 function getNumber(element_name){
     if(isNaN(element_name.value)){
         return 0;
@@ -22,6 +33,16 @@ function getNumber(element_name){
     }
 }
 
+// This function is the main meat of the the program, it calculates the amount of bills/coins to take out based
+// on if taking out a single item will cause that item amount to equal zero, or taking out a single item will make
+// the amount you are counting to zero otherwise it will increment a counter, and it will do this until one of the two happens
+// Psudo Code:
+// if dollar/coin - total value == 0 or dollar/coin - amount of dollar/coin{
+//      return
+// }
+// else {
+//      increment counter
+// }
 function takeOut(total_cash_amount, total_number_of_bills, dollar_value_of_bills, total_value) {
     let counter = 0;
 
@@ -37,6 +58,9 @@ function takeOut(total_cash_amount, total_number_of_bills, dollar_value_of_bills
     return counter;
   }
 
+// This calculates the total values of all the amounts of bills and coins you have
+// I.E. - 4 quarters * 0.25 = 1.00
+// It does that for everything in the array at the top
   function calculateTotal(){
       let total = 0.00;
       
@@ -53,6 +77,8 @@ function takeOut(total_cash_amount, total_number_of_bills, dollar_value_of_bills
       calculateOut(total)
   }
 
+// This is the main display function, it controls assigning values and calling the helper functions
+// to calculate everything that needs to be taken out based on the user input.
   function calculateOut(total){
     let count_to = parseFloat(grabElement("count_to").value);
 
@@ -63,7 +89,12 @@ function takeOut(total_cash_amount, total_number_of_bills, dollar_value_of_bills
     }
 
     grabElement("adj_total").value = total.toFixed(2);
-
+    
+    // This is a bit of manual error checking
+    // because of the way JavaScript handles floating point calculations and rounding
+    // sometimes at the end a single penny would be left over when it needed to be removed
+    // so this checks if that is happening and changes values based on that
+    // I left the console log in there so you can see when it is happening
     if(grabElement("adj_total").value == grabElement("count_to").value + ".01"){
         grabElement(id_names[7] + "_out").value = parseInt(grabElement(id_names[7] + "_out").value) + 1;
         grabElement("adj_total").value = (parseFloat(grabElement("adj_total").value) - 0.01).toFixed(2);
@@ -71,6 +102,9 @@ function takeOut(total_cash_amount, total_number_of_bills, dollar_value_of_bills
     }
   }
 
+
+// This function puts in random numbers into the input boxes for testing purposes
+// using random numbers from the array
   function populateContent(){
     let maxNumber = [15, 10, 20, 50, 20, 30, 20, 50, 2, 1, 1, 1];
     
